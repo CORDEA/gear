dd700531c38e517be0f4f9fd8da9cb41cf2fa28c
Yoshihiro Tanaka <contact@cordea.jp>
2020-03-20 19:11:52 +0900

    execute command

--- gear.go
+++ gear.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 type command interface {
 	equals(source string) bool
-	exec()
+	exec(args []string)
 }
 
 type commit struct {
@@ -17,7 +20,17 @@ func (c *commit) equals(source string) bool {
 	return source == "c"
 }
 
-func (c *commit) exec() {
+func (c *commit) exec(args []string) {
+	cmd := exec.Command("git", append([]string{"commit"}, args...)...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalln("Failed to execute command. ", err)
+	}
+}
+
+func parseCommand(cmd string) []string {
+	return strings.Split(strings.TrimSpace(cmd), " ")
 }
 
 func main() {
@@ -26,10 +39,11 @@ func main() {
 		&commit{},
 	}
 	for {
-		text, _:= reader.ReadString('\n')
+		text, _ := reader.ReadString('\n')
+		parsed := parseCommand(text)
 		for _, cmd := range commands {
-			if cmd.equals(text) {
-				cmd.exec()
+			if cmd.equals(parsed[0]) {
+				cmd.exec(parsed[1:])
 				continue
 			}
 		}
