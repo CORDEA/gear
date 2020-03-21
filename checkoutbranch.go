115a21f623799215fd824f136960da093d13e54f
Yoshihiro Tanaka <contact@cordea.jp>
2020-03-21 15:04:17 +0900

    define new command

---
+++ checkoutbranch.go
@@ -0,0 +1,12 @@
+package main
+
+type CheckoutBranch struct {
+}
+
+func (c *CheckoutBranch) Equals(source string) bool {
+	return source == "cb" || source == "bb"
+}
+
+func (c *CheckoutBranch) Exec(command []string) {
+	ExecGitCommand(append([]string{"checkout", "-b"}, command[1:]...))
+}
--- gear.go
+++ gear.go
@@ -26,6 +26,7 @@ func main() {
 		&Log{},
 		&Checkout{},
 		&Branch{},
+		&CheckoutBranch{},
 	}
 	for {
 		fmt.Print("> ")
